40d9314da8bfaa684af7a604a03099320fe01a88
Damian Kupiturski <keylopl@gmail.com>
2021-05-10 22:43:36 +0200

    opcja dodania ksiazki do bazy danych

--- biblioteka.py
+++ biblioteka.py
@@ -51,8 +51,24 @@ def clear_fields():
 	rok_wydania_box.delete(0, END)
 
 
+# dodanie ksiazki do bazy
+def add_book():
+	sql_command = "INSERT INTO ksiazki (tytul, imie_autora, nazwisko_autora, rok_wydania) VALUES (%s, %s, %s, %s)"
+	values = (tytul_box.get(), imie_autora_box.get(), nazwisko_autora_box.get(), rok_wydania_box.get())
+	my_cursor.execute(sql_command, values)
+
+	# commit do bazy
+	mydb.commit()
+	# czyszczenie pol
+	clear_fields()
+    
 
 
+
+
+# tworzenie etykiety
+title_label = Label(root, text="Baza ksiazkowa", font=("Helvetica", 16))
+title_label.grid(row=0, column=0, columnspan=2, pady=10)
 
 #glowny formularz do dodawania danych ksiazek
 tytul_label = Label(root, text="Tytuł").grid(row=1, column=0, sticky=W, padx=10)
@@ -73,7 +89,9 @@ nazwisko_autora_box.grid(row=3, column=1, pady=5)
 rok_wydania_box = Entry(root)
 rok_wydania_box.grid(row=4, column=1, pady=5)
 
-
+# guziki do dodawania ksiazek
+add_book_button = Button(root, text="Dodaj książke do bazy", command=add_book)
+add_book_button.grid(row=14, column=0, padx=10, pady=10)
 clear_fields_button = Button(root, text="Wyczyść pola", command=clear_fields)
 clear_fields_button.grid(row=14, column=1)
 
