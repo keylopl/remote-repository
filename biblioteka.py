1d6c0afb8290eddc012fba9180472fec34c40d7f
Damian Kupiturski <keylopl@gmail.com>
2021-05-10 22:00:50 +0200

    tworzenie tabeli biblioteka

--- biblioteka.py
+++ biblioteka.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import mysql.connector
-
+import csv
+from tkinter import ttk
 
 root = Tk()
 root.geometry("350x600")
@@ -11,10 +12,22 @@ mydb = mysql.connector.connect(
 		host = "localhost",
 		user = "root",
 		passwd = "admin",
+		database = "biblioteka",
 	)
 
-sprawdzenie czy polaczylismy sie z baza danych
-print(mydb)
+# sprawdzenie czy polaczylismy sie z baza danych
+# print(mydb)
+
+# tworzenie cursora
+my_cursor = mydb.cursor()
+
+# tworzenie database
+my_cursor.execute("CREATE DATABASE biblioteka")
+
+# sprawdzenie czy stworzylismy biblioteke
+my_cursor.execute("SHOW DATABASES")
+for db in my_cursor:
+	print(db)
 
 
 root.mainloop()
